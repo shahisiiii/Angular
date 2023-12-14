8b8e9b0c3a233ddeadbabc68014b364907a9aa21
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-14 09:47:49 +0530

    yesterday pending work competed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Day1Component } from './day1/day1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"d1",component:Day1Component},
+];
